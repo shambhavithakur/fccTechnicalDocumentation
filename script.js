213dda530194f3dc87df8ea3a28d71828bfc5c45
Shambhavi <reply2112@gmail.com>
2018-11-21 16:15:45 +0530

    Configure navigation for small screens and adjust space around sections

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
+// Open-close navigation list
+var togglerOpen = document.querySelector(".open-button");
+var navListOpen = document.querySelector("#navbar");
+var getOverlay = document.querySelector(".overlay");
 var getNavLinks = document.querySelectorAll(".nav-link");
+
+function openNavlist(event) {
+  event.preventDefault();
+  togglerOpen.classList.toggle("open-list");
+  navListOpen.classList.toggle("navlist-open");
+  getOverlay.classList.toggle("overlay-visible");
+}
+
+function closeNavlist(event) {
+  event.preventDefault();
+  togglerOpen.classList.remove("open-list");
+  navListOpen.classList.remove("navlist-open");
+  getOverlay.classList.remove("overlay-visible");
+}
+
+togglerOpen.addEventListener("click", openNavlist);
+getOverlay.addEventListener("click", closeNavlist);
+
+function scaleIncrease() {
+  this.style.transform = "scale3d(1, 1, 1)";
+}
+
+function scaleDecrease() {
+  this.style.transform = "scale3d(.88, .88, .88)";
+}
+
+togglerOpen.addEventListener("mouseout", scaleIncrease);
+togglerOpen.addEventListener("mouseover", scaleDecrease);
 
 for (var i = 0; i < getNavLinks.length; i++) {
   getNavLinks[i].addEventListener("click", function() {
@@ -6,5 +38,8 @@ for (var i = 0; i < getNavLinks.length; i++) {
       getNavLinks[j].classList.remove("active-link");
     }
     this.classList.add("active-link");
+    togglerOpen.classList.remove("open-list");
+    navListOpen.classList.remove("navlist-open");
+    getOverlay.classList.remove("overlay-visible");
   });
 }
